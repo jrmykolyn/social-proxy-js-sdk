9d4ad788958ddd82b3b4294028c530f727ccd1ba
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-09-22 09:38:35 -0400

    Add 'src/modules/' dir. Move 'InstagramModule' into dedicated file.

---
+++ src/modules/instagram/index.ts
@@ -0,0 +1,38 @@
+export class InstagramModule {
+	constructor() {
+
+	}
+
+	// INSTANCE METHODS
+	/**
+	 * Function fetches a given Instagram feed from the Social Proxy API, returns the result as a Promise
+	 *
+	 * @param {string} handle - The Instagram handle/feed to fetch.
+	 * @param {Object} options
+	 * @return {Promise}
+	 */
+	fetch( handle: string, options: any = {} ) {
+		return new Promise( ( resolve, reject ) => {
+			var query = options.query || {};
+			var queryString = Object.keys( query ).map( ( key ) => { return `${key}=${query[ key ]}`; } ).join( '&' );
+
+			var req = new XMLHttpRequest();
+
+			req.open( 'GET', `https://social-proxy.herokuapp.com/instagram/${handle}?${queryString}` );
+
+			req.onreadystatechange = function() {
+				if ( this.readyState === 4 ) {
+					var status: any = this.status;
+
+					if ( parseInt( status ) === 200 ) {
+						resolve( this.response );
+					} else {
+						reject( this.response || this.statusText );
+					}
+				}
+			}
+
+			req.send();
+		} );
+	}
+}
