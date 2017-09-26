8b1203d9c9b6edc4b5d4d322e0ffa26f97ffc3a6
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-09-25 22:21:20 -0400

    Add 'getCacheKey()' and 'getCache()' methods to 'CacheModule'. Refactor existing methods.

--- src/modules/cache/index.ts
+++ src/modules/cache/index.ts
@@ -16,7 +16,7 @@ export class CacheModule implements ModuleInterface {
 	 * @param {number} ttl
 	 * @return {number|null}
 	 */
-	sanitizeTtl( ttl: number ) {
+	sanitizeTtl( ttl: number ): any {
 		return ( ttl && typeof ttl === 'number' ) ? ( new Date().getTime() + ttl ) : null;
 
 	}
@@ -26,7 +26,7 @@ export class CacheModule implements ModuleInterface {
 	 *
 	 * @return {number}
 	 */
-	getDefaultTtl() {
+	getDefaultTtl(): number {
 		return ( new Date().getTime() + ( 1000 * 60 * 60 * 24 * 7 ) );
 	}
 
@@ -35,8 +35,44 @@ export class CacheModule implements ModuleInterface {
 	 *
 	 * @return {string}
 	 */
-	getCachePrefix() {
+	getCachePrefix(): string {
 		return 'socialProxy';
+	}
+
+	/**
+	 * ...
+	 *
+	 * @param {Object} options
+	 */
+	getCacheKey( options: any = {} ): any {
+		var platform = options.platform;
+		var handle = options.handle;
+		var url = options.url;
+
+		if ( platform && handle && url ) {
+			return `${this.getCachePrefix()}__${platform}__${handle}__${url}`;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * ...
+	 *
+	 * @param {Object} options
+	 * @param {Mixed|null}
+	 */
+	getCache( options: any = {} ): any {
+		var key = this.getCacheKey( options );
+		var data = null;
+
+		if ( !key ) {
+			return null;
+		}
+
+		data = window.localStorage.getItem( key );
+
+		return ( data ) ? JSON.parse( data ) : data;
 	}
 
 	/**
@@ -45,23 +81,19 @@ export class CacheModule implements ModuleInterface {
 	 * @param {Object} data
 	 * @param {Object} options
 	 */
-	setCache( data: any, options: any ) {
+	setCache( data: any, options: any ): any {
 		if ( !window.localStorage ) {
 			/// TODO[@jrmykolyn]: Print warning message.
 			return;
 		}
 
-		const platform = options.platform;
-		const handle = options.handle;
-		const url = options.url;
+		const key = this.getCacheKey( options );
 		const ttl = this.sanitizeTtl( options.ttl ) || this.getDefaultTtl();
 
 		// Update data with `socialProxy` client info.
 		data.socialProxy = {
 			ttl: ttl,
 		};
-
-		var key = `${this.getCachePrefix()}__${platform}__${handle}__${url}`;
 
 		window.localStorage.setItem( key, JSON.stringify( data ) );
 	}
