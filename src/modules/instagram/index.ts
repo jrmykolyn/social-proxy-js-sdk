ee04b8b9a5ac3c863d7223acca833a93e31674af
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-12-21 09:05:08 -0500

    topic-3 Update 'InstagramModule': replace 'XMLHttpRequest' with 'fetch'.

--- src/modules/instagram/index.ts
+++ src/modules/instagram/index.ts
@@ -20,8 +20,6 @@ export class InstagramModule implements ModuleInterface {
 	 */
 	fetch( handle: string, options: any = {} ) {
 		return new Promise( ( resolve, reject ) => {
-			var _this = this;
-
 			var query = options.query || {};
 			var ttl = options.ttl || null;
 			var queryString = Object.keys( query ).map( ( key ) => { return `${key}=${query[ key ]}`; } ).join( '&' );
@@ -30,9 +28,7 @@ export class InstagramModule implements ModuleInterface {
 			var url = `${endpoints.instagram}/${handle}?${queryString}`;
 
 			// Check if requested data has been fetched/cached.
-			var cachedData = _this.ref.cache.getCache( { platform: 'instagram', handle, url } );
-
-			var req = null;
+			var cachedData = this.ref.cache.getCache( { platform: 'instagram', handle, url } );
 
 			// Return cached data if:
 			// - 'bust cache' set to false.
@@ -42,34 +38,34 @@ export class InstagramModule implements ModuleInterface {
 			if ( !options.bustCache && cachedData && !cachedData.socialProxy.isExpired ) {
 				resolve( cachedData );
 				return;
+			// Otherwise, fetch new data.
 			} else {
-				req = new XMLHttpRequest();
-
-				req.open( 'GET', url );
-
-				req.onreadystatechange = function() {
-					if ( this.readyState === 4 ) {
-						var status: any = this.status;
-
-						if ( parseInt( status ) === 200 ) {
-
-							if ( !!options.cache ) {
-								_this.ref.cache.setCache( JSON.parse( this.response ), {
-									platform: 'instagram',
-									handle,
-									url,
-									ttl,
-								} );
-							}
-
-							resolve( JSON.parse( this.response ) );
-						} else {
-							reject( this.response || this.statusText );
+				fetch( url )
+					.then( ( response ) => {
+						return response.json();
+					} )
+					.then( ( data ) => {
+						// Reject if error.
+						if ( data.error ) {
+							reject( data );
+							return;
 						}
-					}
-				}
 
-				req.send();
+						// Cache if applicable.
+						if ( !!options.cache ) {
+							this.ref.cache.setCache( data, {
+								platform: 'instagram',
+								handle,
+								url,
+								ttl,
+							} );
+						}
+
+						resolve( data );
+					} )
+					.catch( ( err ) => {
+						reject( err ); /// TODO: Ensure that this includes meaningful info/has same 'shape' as other errors.
+					}  );
 			}
 		} );
 	}
